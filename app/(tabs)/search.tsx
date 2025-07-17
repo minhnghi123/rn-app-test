d0a9b83e03cb8fa1cebf941ccda9b774d239616f
Alien <minhhnghiii@gmail.com>
2025-07-17 18:25:25 +0700

    feat(search): debounce search

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,14 +1,101 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import { useFetch } from "@/services/useFetch";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    refetch: refetchMovies,
+    reset: resetMovies,
+  } = useFetch(
+    () =>
+      fetchMovies({
+        query: searchQuery.trim(),
+      }),
+    false
+  );
+  // using debounce search
+  useEffect(() => {
+    const timeOutId = setTimeout(async () => {
+      if (searchQuery.trim()) await refetchMovies();
+      else resetMovies();
+    }, 500);
+    return () => clearTimeout(timeOutId);
+  }, [searchQuery]);
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0 "
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 10,
+          marginTop: 20,
+        }}
+        ListEmptyComponent={
+          !moviesLoading && !moviesError ? (
+            <Text className="text-white text-center px-5 mt-10">
+              {searchQuery.trim()
+                ? "No results found"
+                : "Start searching for movies"}
+            </Text>
+          ) : null
+        }
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search Movies ..."
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
+            </View>
+            {moviesLoading && (
+              <ActivityIndicator
+                size="large"
+                color="#0000ff"
+                className="my-3"
+              />
+            )}
+            {moviesError && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {moviesError.message}
+              </Text>
+            )}
+            {!moviesLoading &&
+              !moviesError &&
+              searchQuery.trim() &&
+              movies?.length > 0 && (
+                <Text className="text-white text-xl font-bold">
+                  Search Results for{" "}
+                  <Text className="text-accent">{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default Search;
